22e13881779824350902556fb48528f9dabb5123
Eduardo Guzman <eduardo.guzman@jalasoft.com>
2023-05-03 00:29:57 -0400

    Add functionality to handle routes.

---
+++ src/components/nav/navbar.component.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import HomeIcon from '@mui/icons-material/Home';
+import Button from '@mui/material/Button';
+import {useNavigate} from 'react-router-dom';
+
+
+const NavBar = () => {
+  const navigate = useNavigate();
+
+  const navigateHome = () => {
+    navigate('/');
+  };
+
+  const navigateToLogin = () => {
+    navigate('/auth/login');
+  };
+
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        position: 'fixed',
+        left: 0,
+        top: 0,
+        width: '100%',
+        height: '40px',
+      }}
+    >
+      <AppBar position="static">
+        <Toolbar style={{ minHeight: 'fit-content' }}>
+          <IconButton onClick={navigateHome} sx={{ mr: 2 }}>
+            <HomeIcon sx={{ color: "white" }} />
+          </IconButton>
+          <Typography variant="h5" color="#FFFFFF" sx={{ flexGrow: 1 }}>
+            TODO APP
+          </Typography>
+          <Button color="inherit" onClick={navigateToLogin}>Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
+
+export default NavBar
--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,13 +1,39 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Box } from "@mui/material";
+import {useNavigate} from 'react-router-dom';
+import Button from '@mui/material/Button';
+import NavBar from "../../components/nav/navbar.component";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const navigateToTodos = () => {
+    navigate('/todos');
+  };
+
   return (
-    <>
-      <div>Home Page</div>
-      <Link to="/todos">go to Todos</Link>
-    </>
+    <Box>
+      <Box>
+        <NavBar />
+      </Box>
+      <Box
+        sx={{
+          position: "fixed",
+          top: "40px",
+          left: 0,
+          width: "100%",
+          height: "100%",
+          alignItems: "center",
+          textAlign: "center",
+          color: "#3F51B5",
+          backgroundColor: "darkgrey",
+        }}
+      >
+        <div>Home Page</div>
+        <Button color="primary" onClick={navigateToTodos}>Todos</Button>
+      </Box>
+    </Box>
   );
-}
+};
 
 export default HomePage;
